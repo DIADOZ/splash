763bcd8c781cefe3f70112db1931604157def22a
gorenzogo@gmail.com <gorenzogo@gmail.com>
2020-02-23 23:25:18 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function showSplash2() {
+    document.getElementById("splash2").style.display ="block";
+    document.getElementById("splash3").style.display ="none";
+    document.getElementById("splash1").style.display ="none";
+}
+
+function showSplash3() {
+    document.getElementById("splash2").style.display ="none";
+    document.getElementById("splash3").style.display ="block";
+    document.getElementById("splash1").style.display ="none";
+}
+
+function showSplash1() {
+    document.getElementById("splash2").style.display ="none";
+    document.getElementById("splash3").style.display ="none";
+    document.getElementById("splash1").style.display ="block";
+}
