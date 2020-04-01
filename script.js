d75a55b6c812a571def48a4eb654104f9d0c22f6
gorenzogo@gmail.com <gorenzogo@gmail.com>
2020-04-01 09:18:46 -0400

    added favicon, changed link to different pages.

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
+function selectVersion() {
+    var x = document.getElementById("splash-versions").value;
+    if (x == 1) {
+        showSplash1();
+    } else if (x == 2) {
+        showSplash2();
+    } else if (x == 3) {
+        showSplash3();
+    }
+}
+
 function showSplash2() {
-    document.getElementById("splash2").style.display ="block";
-    document.getElementById("splash3").style.display ="none";
-    document.getElementById("splash1").style.display ="none";
+    document.getElementById("splash2").style.display = "block";
+    document.getElementById("splash3").style.display = "none";
+    document.getElementById("splash1").style.display = "none";
 }
 
 function showSplash3() {
-    document.getElementById("splash2").style.display ="none";
-    document.getElementById("splash3").style.display ="block";
-    document.getElementById("splash1").style.display ="none";
+    document.getElementById("splash2").style.display = "none";
+    document.getElementById("splash3").style.display = "block";
+    document.getElementById("splash1").style.display = "none";
 }
 
 function showSplash1() {
-    document.getElementById("splash2").style.display ="none";
-    document.getElementById("splash3").style.display ="none";
-    document.getElementById("splash1").style.display ="block";
+    document.getElementById("splash2").style.display = "none";
+    document.getElementById("splash3").style.display = "none";
+    document.getElementById("splash1").style.display = "block";
 }
