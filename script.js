402d527b3d055f3345d8d280d0bf02e60fbe1454
gorenzogo@gmail.com <gorenzogo@gmail.com>
2020-04-14 15:57:44 -0400

    Replaced menu w/ buttons. Changed intro text.

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ function selectVersion() {
     }
 }
 
+function showSplash1() {
+    document.getElementById("splash2").style.display = "none";
+    document.getElementById("splash3").style.display = "none";
+    document.getElementById("splash1").style.display = "block";
+}
+
+
 function showSplash2() {
     document.getElementById("splash2").style.display = "block";
     document.getElementById("splash3").style.display = "none";
@@ -21,9 +28,4 @@ function showSplash3() {
     document.getElementById("splash1").style.display = "none";
 }
 
-function showSplash1() {
-    document.getElementById("splash2").style.display = "none";
-    document.getElementById("splash3").style.display = "none";
-    document.getElementById("splash1").style.display = "block";
-}
 
