a9889122583ee28fe1fb3966527ba35222e6ba60
gorenzogo@gmail.com <gorenzogo@gmail.com>
2020-04-17 15:22:38 -0400

    added fade in functionality. needs work.

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+$(window).on("load", function() {
+    $(window)
+      .scroll(function() {
+        var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+        $(".body-unit").each(function() {
+          /* Check the location of each desired element */
+          var objectTop = $(this).offset().top;
+  
+          /* If the element's top is inside the viewport (plus 150px for padding) then fade it in */
+          if (objectTop < windowBottom - 120) {
+            //object comes into view (scrolling down)
+            $(this).addClass("fade");
+          }
+        });
+      })
+      .scroll(); //invoke scroll-handler on page-load
+  });
+
 function selectVersion() {
     var x = document.getElementById("splash-versions").value;
     if (x == 1) {
@@ -29,3 +47,5 @@ function showSplash3() {
 }
 
 
+
+  
